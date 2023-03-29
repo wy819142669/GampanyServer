e82f4d9aed0d3adc4e7d5cb9157994ddab6510ac
imzdj <imzdj@126.com>
2023-03-29 13:40:43 +0800

    提交打印信息给王总看

--- SandTableClient/Assets/Game/Scripts/WebService.cs
+++ SandTableClient/Assets/Game/Scripts/WebService.cs
@@ -28,24 +28,31 @@ public class WebService : MonoBehaviour
 
     public int WebRequest(string url, string body)
     {
-        StartCoroutine(asycWebRequet(url, body, _requestID));
+        Debug.Log("WebRequest" + 0);
+        asycWebRequet(url, body, _requestID);
+        Debug.Log("WebRequest" + 1);
         return _requestID++;
     }
 
     IEnumerator asycWebRequet(string url, string param, int requestID)
     {
+        Debug.Log("asycWebRequet" + 0);
         WWW www = new WWW(url, Encoding.UTF8.GetBytes(param));
+        Debug.Log("asycWebRequet"+1);
         yield return www;
+        Debug.Log("asycWebRequet" + 2);
         if (!String.IsNullOrEmpty(www.error))
         {
             Debug.LogErrorFormat("asycWebRequet failed: network error {0}", www.error);
             yield break;
         }
-        System.Threading.Thread.Sleep(1000);    
+        Debug.Log("asycWebRequet" + 3);
         string body = www.text;
+        Debug.Log("asycWebRequet" + 4);
         www.Dispose();
 
-        //Debug.Log(body);
+        Debug.Log("asycWebRequet" + 5);
+        // Debug.Log(body);
         GameClient.CallLua("Client:OnWebRespond", body);
     }
 }
